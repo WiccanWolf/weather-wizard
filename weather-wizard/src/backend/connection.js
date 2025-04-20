c37867ccf7fd20b36d46d19dfdaf9bbbe91fd8a8
Toni <dayhlprod@gmail.com>
2025-04-20 20:29:01 +0100

    Initialised project

---
+++ weather-wizard/src/backend/connection.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+import cors from 'cors';
+dotenv.config();
+
+const app = express();
+const port = 3752;
+
+const baseUrl = 'http://api.weatherapi.com/v1';
+
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'Hello world!' });
+});
+
+app.get('/current', async (req, res) => {
+  const { q } = req.query;
+  const key = process.env.WEATHER_API_KEY;
+
+  if (!q) {
+    return res.status(400).send({ error: 'Missing required query parameter.' });
+  }
+  try {
+    const response = await axios.get(`${baseUrl}/current.json`, {
+      params: { key, q },
+    });
+    res.status(200).send(response.data);
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).send({ error: 'Failed to fetch weather data.' });
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ weather-wizard/src/components/AppDesign.jsx
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const AppDesign = () => {
+  const fetchWeather = async () => {
+    try {
+      const response = await axios.get();
+    } catch (error) {
+    } finally {
+    }
+  };
+};
+
+export default AppDesign;
